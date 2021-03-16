f7d02e9cb8d5391a9a956f38938f26e3ab38359f
stevey-sy <sinsy89@hanmail.net>
2021-03-16 13:53:27 +0900

    LiveData 사용하여 db 변화생기면 자동으로 UI 갱신하기

--- app/src/main/java/com/example/roomexamplejava/MainActivity.java
+++ app/src/main/java/com/example/roomexamplejava/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.roomexamplejava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mTodoEditText;
@@ -28,13 +31,21 @@ public class MainActivity extends AppCompatActivity {
                 .allowMainThreadQueries()
                 .build();
 
-        mResultTextView.setText(db.todoDao().getAll().toString());
+        // live data 를 사용해서 db의 내용을 관찰하고
+        // 변화가 있으면 자동으로 UI를 갱신한다.
+        db.todoDao().getAll().observe(this, new Observer<List<Todo>>(){
+            @Override
+            public void onChanged(List<Todo> todos) {
+                mResultTextView.setText(todos.toString());
+            }
+        });
 
+        // 버튼 클릭시 db에 insert 한다.
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-                mResultTextView.setText(db.todoDao().getAll().toString());
+//                mResultTextView.setText(db.todoDao().getAll().toString());
             }
         });
 
--- app/src/main/java/com/example/roomexamplejava/TodoDao.java
+++ app/src/main/java/com/example/roomexamplejava/TodoDao.java
@@ -1,5 +1,6 @@
 package com.example.roomexamplejava;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -14,7 +15,7 @@ import java.util.List;
 @Dao
 public interface TodoDao {
     @Query("SELECT * FROM Todo")
-    List<Todo> getAll();
+    LiveData<List<Todo>> getAll();
 
     @Insert
     void insert(Todo todo);
